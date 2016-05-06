cdf652553ad8dc1c6e2286f96031038ce4e52987
SC4100E <SC4100E@lindenwood.edu>
2016-05-06 10:28:22 -0500

    remove script to see if thats the issue

--- scripts/main.js
+++ scripts/main.js
@@ -67,16 +67,6 @@ $(document).ready(function() {
 
 	// animations
 
-	$(window).scroll(function() {
-		$('.imgContainer').each(function(){
-		var imagePos = $(this).offset().top;
-
-		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+400) {
-				$(this).addClass("fadeIn");
-			}
-		});
-	});
 
 
 
